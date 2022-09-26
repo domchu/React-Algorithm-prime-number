350095c40a3cac451b75b48f748c3de2417d2847
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-26 22:49:12 +0100

    chore:added the structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
+// import react from "react";
 
 function App() {
   return (
-    <div className="App">
-      <h2>Prime Number</h2>
-    </div>
+    <>
+      <div className="App">
+        <h2>Prime Number</h2>
+      </div>
+      <form action="">
+        <label htmlFor="num">Your Prime:</label>
+        <input
+          type="number"
+          value={selectedNum}
+          onChange={(event) => {
+            let num = Math.min(100_000, Number(event.target.value));
+          }}
+        />
+      </form>
+    </>
   );
 }
 
