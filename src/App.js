02295d92113ff02bda06776f35e3935ea60919c4
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-09-26 22:59:53 +0100

    text: written the statement

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
-// import react from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [selectedNum, setSelectedNum] = useState(100);
+
+  const allPrimes = [];
+  for (let counter = 2; counter < selectedNum; counter++) {
+    if (isPrime(counter)) {
+      allPrimes.push(counter);
+    }
+  }
   return (
     <>
       <div className="App">
@@ -14,9 +22,14 @@ function App() {
           value={selectedNum}
           onChange={(event) => {
             let num = Math.min(100_000, Number(event.target.value));
+            setSelectedNum(num);
           }}
         />
       </form>
+      <p>
+        there are {allPrimes.length} prime(s) between 1 and {selectedNum}:{" "}
+        <span>{allPrimes.join(",")}</span>
+      </p>
     </>
   );
 }
